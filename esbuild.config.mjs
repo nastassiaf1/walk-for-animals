98a763629fa97d58131851ea8fe66acce1e9c315
nastassiaf1 <nastassia.fiadotava1@gmail.com>
2024-03-12 21:28:45 +0400

    add eslint config & esbuild settings

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -1,14 +1,15 @@
-import { build } from 'esbuild';
 import sassPlugin from 'esbuild-plugin-sass';
 import cssModulesPlugin from 'esbuild-css-modules-plugin';
 
-build({
+export default {
     entryPoints: ['src/index.tsx'],
     bundle: true,
+    minify: false,
     outdir: 'dist',
     plugins: [sassPlugin(), cssModulesPlugin()],
     loader: {
         '.ts': 'ts',
         '.tsx': 'tsx',
     },
-}).catch(() => process.exit(1));
+    tsconfig: 'tsconfig.json',
+};
