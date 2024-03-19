087292c97c45d82b0b1f3c33d7baf84dc7475d90
nastassiaf1 <nastassia.fiadotava1@gmail.com>
2024-03-19 17:57:39 +0400

    provide store

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,13 @@
 import { createRoot } from 'react-dom/client';
 import router from './router';
 import { RouterProvider } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from 'store/store';
 
 const root = createRoot(document.getElementById('app'));
 
-root.render(<RouterProvider router={router}></RouterProvider>);
+root.render(
+    <Provider store={store}>
+        <RouterProvider router={router}></RouterProvider>
+    </Provider>
+);
