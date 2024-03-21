e8e703b1c69d7c5e55c244b38d500ab526ee01a2
nastassiaf1 <nastassia.fiadotava1@gmail.com>
2024-03-21 16:53:58 +0400

    inplement dropdown for nav

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,42 +1,63 @@
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
-import navStyle from './../styles/nav.module.scss';
+import styles from './../styles/nav.module.scss';
 
 export default function Navigation() {
     return (
-        <nav className={navStyle.nav} aria-label="Main Navigation">
-            <NavLink
-                to="/events"
-                className={({ isActive, isPending }) =>
-                    `${navStyle.navItem} ${isPending ? navStyle.pending : ''} ${isActive ? navStyle.active : ''}`
-                }
-            >
-                Event Info
-            </NavLink>
-            <NavLink
-                to="/fundraising"
-                className={({ isActive, isPending }) =>
-                    `${navStyle.navItem} ${isPending ? navStyle.pending : ''} ${isActive ? navStyle.active : ''}`
-                }
-            >
-                Fundraising
-            </NavLink>
-            <NavLink
-                to="/prizes"
-                className={({ isActive, isPending }) =>
-                    `${navStyle.navItem} ${isPending ? navStyle.pending : ''} ${isActive ? navStyle.active : ''}`
-                }
-            >
-                Prizes
-            </NavLink>
+        <nav className={styles.nav} aria-label="Main Navigation">
+            <div className={styles.navItem}>
+                <NavLink
+                    to="/events"
+                    className={({ isActive, isPending }) =>
+                        `${styles.navItemLink} ${isPending ? styles.pending : ''} ${isActive ? styles.active : ''}`
+                    }
+                >
+                    Event Info
+                </NavLink>
+                <div className={styles.navDropdown}>
+                    <Link to="/events">General info</Link>
+                    <Link to="/wfavendors">Food &amp; retail</Link>
+                    <Link to="/events">Parking</Link>
+                    <Link to="/events">Event FAQs</Link>
+                    <Link to="/events">Event partners</Link>
+                </div>
+            </div>
+
+            <div className={styles.navItem}>
+                <NavLink
+                    to="/fundraising"
+                    className={({ isActive, isPending }) =>
+                        `${styles.navItemLink} ${isPending ? styles.pending : ''} ${isActive ? styles.active : ''}`
+                    }
+                >
+                    Fundraising
+                </NavLink>
+                <div className={styles.navDropdown}>
+                    <Link to="/events">Fundraising tips & tools</Link>
+                    <Link to="/events">Matching gifts</Link>
+                    <Link to="/events">Teams</Link>
+                </div>
+            </div>
+
+            <div className={styles.navItem}>
+                <NavLink
+                    to="/prizes"
+                    className={({ isActive, isPending }) =>
+                        `${styles.navItemLink} ${isPending ? styles.pending : ''} ${isActive ? styles.active : ''}`
+                    }
+                >
+                    Prizes
+                </NavLink>
+            </div>
+
             <button
-                className={`${navStyle.button} ${navStyle.buttonLighter}`}
+                className={`${styles.button} ${styles.buttonLighter}`}
                 type="button"
             >
                 Register
             </button>
             <button
-                className={`${navStyle.button} ${navStyle.buttonDarker}`}
+                className={`${styles.button} ${styles.buttonDarker}`}
                 type="button"
             >
                 Donate
