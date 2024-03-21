8d3d9c2f7d1a78a7431c55d9a07efb3df080e69c
nastassiaf1 <nastassia.fiadotava1@gmail.com>
2024-03-21 19:00:36 +0400

    implement accordion

---
+++ src/constants/faqs.tsx
@@ -0,0 +1,165 @@
+const FaqsData = [
+    {
+        question: 'Do I need to fundraise to attend the Walk for Animals?',
+        answer: (
+            <p>
+                All animal lovers are welcome at the Walk for Animals whether
+                they have fundraised or not.&nbsp;To safeguard all our
+                constituents against spam and phishing, TeamRaiser now requires
+                a minimum of $5 raised to send emails from your Participant
+                Center.
+            </p>
+        ),
+    },
+    {
+        question: 'Is it free to attend the Walk for Animals?',
+        answer: (
+            <p>
+                While there’s no admission fee, the Walk for Animals is Animal
+                Humane Society’s largest and most important fundraising event of
+                the year, helping to make thousands of new beginnings possible
+                for animals in need each year. The suggested donation for
+                non-fundraisers is $20 per person.
+            </p>
+        ),
+    },
+    {
+        question: 'How do I customize my personal fundraising page?',
+        answer: (
+            <>
+                <p>
+                    When logged in to your Participant Center, scroll down to
+                    the section titled “Personal Page.” Here you’ll see the
+                    option to edit your personal page content, including the
+                    page title, body copy, and photo. If you’re part of a team,
+                    you’ll also have the option to edit your team page content
+                    in this area. Just be sure you’re working in the “My Team”
+                    tab to edit team content and fundraising goals.
+                </p>
+                <img
+                    src="https://secure2.convio.net/ahs/images/content/pagebuilder/Participant-Center-Edit-Content-1.jpg?t=1709237044646"
+                    alt="Editing your participant center"
+                />
+
+                <p>
+                    You’ll also see your personal page URL. Select the “URL
+                    Settings” button to create a personalized URL if you’d like.
+                </p>
+
+                <p>
+                    Also toward the bottom of your Participant Center you’ll
+                    find the option to edit your fundraising goal.
+                </p>
+
+                <img
+                    src="https://secure2.convio.net/ahs/images/content/pagebuilder/Participant-Center-Fundraising-Goal.jpg?t=1709237252013"
+                    alt="Editing your fundraising goal."
+                />
+            </>
+        ),
+    },
+    {
+        question: 'How do I fundraise for the Walk on Facebook?',
+        answer: (
+            <>
+                <p>
+                    Sharing your fundraiser on Facebook is quick and easy. From
+                    your Participant Center, locate the dark blue box that reads
+                    “Reach your goal faster” and select the “Edit fundraising
+                    content” button.
+                </p>
+
+                <img
+                    src="https://secure2.convio.net/ahs/images/content/pagebuilder/Participant-Center-Connect-to-FB.png?t=1709237501167"
+                    alt="Sharing your fundraiser on Facebook."
+                />
+
+                <p>
+                    A pop-up should appear stating that the Luminate Integration
+                    App is requesting access to manage your fundraiser. Follow
+                    the prompts to grant access, and you’ll also have the option
+                    to edit the title and content of your fundraiser.{' '}
+                </p>
+
+                <img
+                    src="https://secure2.convio.net/ahs/images/content/pagebuilder/Facebook-Fundraiser-Permissions.png?t=1709237733956"
+                    alt="Facebook fundraiser permission."
+                />
+
+                <p>
+                    In the dark blue box in your Participant Center, you’ll now
+                    see that your fundraiser has successfully been connected to
+                    Facebook. From this box, you can navigate directly to your
+                    Facebook fundraiser, or copy the unique URL for your
+                    Facebook fundraiser. Please note: To grant Facebook access,
+                    you’ll need to have pop-ups enabled on your web browser.{' '}
+                </p>
+
+                <p>
+                    Don’t forget — there are several graphics available for you
+                    to share on social media which can be found in our{' '}
+                    <a href="https://secure.animalhumanesociety.org/site/SPageNavigator/WalkforAnimals/wfatoolkit.html">
+                        digital fundraising toolkit.{' '}
+                    </a>
+                </p>
+            </>
+        ),
+    },
+    {
+        question: 'How do I send emails from my Participant Center?',
+        answer: (
+            <>
+                <p>
+                    We want to help make fundraising easy with helpful tools and
+                    support. That’s why you’ll find several ready-to-use, easily
+                    customizable email templates in your Participant Center.
+                </p>
+
+                <p>
+                    You’ll want to start by adding contacts to your address
+                    book. When logged in, select #2 in the list of “What to do
+                    next.” Here you can add contacts individually, or import a
+                    group of contacts. If you’re a returning walker, you’ll see
+                    contacts and prior donors saved here already.{' '}
+                </p>
+
+                <img
+                    src="https://secure2.convio.net/ahs/images/content/pagebuilder/Participant-Center-What-Next.jpg?t=1709238101295"
+                    alt="What to do next."
+                />
+
+                <p>
+                    Now you’re ready to email your friends, family, and
+                    coworkers! From the contacts page, you can select the email
+                    addresses you’d like to contact in bulk. To return to the
+                    email page in the future, select #3 in the list of “What to
+                    do next” when you login, or navigate to the “Email” tab.
+                </p>
+
+                <p>
+                    Choose from several email templates, including three
+                    requests for support and one thank you. Each template can be
+                    customized however you’d like, or you can choose to create
+                    your own from scratch. Then select an email header to
+                    accompany your email. From here you can save your customized
+                    template for future use, or simply select the “Preview &amp;
+                    Send” button to review your email and then send it on to
+                    your recipients.{' '}
+                </p>
+
+                <img
+                    src="https://secure2.convio.net/ahs/images/content/pagebuilder/Participant-Center-Compose-Email.png?t=1709238235021"
+                    alt="Composing an email."
+                />
+
+                <p>
+                    All emails sent from your Participant Center will
+                    automatically include a direct link to your personal page at
+                    the bottom of the email.{' '}
+                </p>
+            </>
+        ),
+    },
+];
+
+export default FaqsData;
