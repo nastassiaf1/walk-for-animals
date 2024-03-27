837874333e49e664f08a26884b1551aa82797110
nastassiaf1 <nastassia.fiadotava1@gmail.com>
2024-03-27 17:53:51 +0400

    update not found page

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,11 +1,17 @@
 import { Link } from 'react-router-dom';
+import styles from './../styles/notfound.module.scss';
+import mainStyles from './../styles/main.module.scss';
 
 export default function NotFoundPage() {
     return (
-        <div>
-            <h1>404 - Page Not Found</h1>
+        <div className={styles.container}>
+            <h3>404 - Page Not Found</h3>
             <p>Sorry, the page you are looking for does not exist.</p>
-            <Link to="/">Go Back Home</Link>
+            <div>
+                <Link to="/" className={mainStyles.link}>
+                    Go Back Home
+                </Link>
+            </div>
         </div>
     );
 }
